5809c09df010b19e040a10989f5376efbc405399
vKongv <kaweng456@hotmail.com>
2015-08-17 14:52:03 +0800

    Implement string tokenizer
    Use string tokenizer to separate line by line.
    Display each line that had been tokenize.

--- main.c
+++ main.c
@@ -9,8 +9,13 @@ char* seperateString;
 fp = popen("/sbin/ifconfig eth0", "r");
 while (fgets(returnData, 128, fp) != NULL)
 {
-
-   printf("%s\n" returnData); //Display "ifconfig" command output
+   /**Tokenize the string from "ifconfig" command output **/
+   char* tokenReturnData = returnData;
+   seperateString = strtok(tokenReturnData, "\n");
+	while (seperateString) {
+    		printf("Token: %s\n", seperateString);
+    		seperateString = strtok(NULL, " ");
+   	}
    
 }
 
